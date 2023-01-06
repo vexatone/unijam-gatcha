e31ff6180e94113777f48ea95a31843a1fd5b3b8
Hunseong Lim <5001945@snu.ac.kr>
2023-01-07 04:57:08 +0900

    Update pause UI

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -3,34 +3,61 @@ using UnityEngine;
 class UIManager : MonoBehaviour
 {
     private bool _paused;
-    [SerializeField] private GameObject button;
-    
+    [SerializeField] private GameObject uiCanvasPanel;
+    [SerializeField] private GameObject pauseButton;
+    [SerializeField] private GameObject pausePopup;
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        _paused = true;
+        // TODO: 스테이지명, 동전 개수, 남은 시간 업데이트
+        uiCanvasPanel.SetActive(true);
+        pausePopup.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        _paused = false;
+        uiCanvasPanel.SetActive(false);
+        pausePopup.SetActive(false);
+    }
+
     public void ShowPause()
     {
-        button.SetActive(true);
+        pauseButton.SetActive(true);
     }
 
     public void HidePause()
     {
-        button.SetActive(false);
+        pauseButton.SetActive(false);
     }
 
     public void OnPauseButtonClick()
     {
-        if (_paused)
-        {
-            Time.timeScale = 1f;
-            _paused = false;
-        }
-        else
-        {
-            Time.timeScale = 0f;
-            _paused = true;
-        }
+        Pause();
     }
+
+    public void OnPausePopupResumeButtonClick()
+    {
+        Resume();
+    }
+
+    // TODO: 나머지 버튼 OnClick 함수 만들기
 
     private void Start()
     {
-        _paused = false;
+        Resume();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_paused)
+                Resume();
+            else
+                Pause();
+        }
     }
 }
