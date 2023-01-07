bb75bc1c554358f64a5a377f14aa8e3a1c0d931b
DESKTOP-KHL63IV\jjun0403 <jjun0403@korea.ac.kr>
2023-01-07 21:19:22 +0900

    Gum

--- Assets/Scripts/TIle/Gum.cs
+++ Assets/Scripts/TIle/Gum.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Gum : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
@@ -12,10 +12,11 @@ public class Gum : MonoBehaviour
             player.jumpable = false;
             player.SetBallState(1.5f, 4.0f);
             player.SetDrag(2f);
+            Debug.Log("asdf");
         }
     }
 
-    private void OnCollisionExit2D(Collision2D col)
+    private void OnTriggerExit2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
