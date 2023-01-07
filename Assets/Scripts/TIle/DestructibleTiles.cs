a727a75872acb08d995eff366f5e4267d075348a
Hunseong Lim <5001945@snu.ac.kr>
2023-01-08 06:57:07 +0900

    Add tile break animation
    little buggy

--- Assets/Scripts/TIle/DestructibleTiles.cs
+++ Assets/Scripts/TIle/DestructibleTiles.cs
@@ -7,9 +7,13 @@ public class DestructibleTiles : MonoBehaviour
 {
     public Tilemap destructableTilemap;
     public bool check = true;
+    private GameObject _brokenTile;
+
     void Start()
     {
         destructableTilemap = GetComponent<Tilemap>();
+        _brokenTile = transform.GetChild(0).gameObject;
+        _brokenTile.SetActive(false);
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -25,6 +29,7 @@ public class DestructibleTiles : MonoBehaviour
                     hitPosition.y = hit.point.y - 0.01f * (hit.normal.y > 0 ? hit.normal.y : hit.normal.y * (-1));
                     check = false;
                     StartCoroutine(BreakTile(hitPosition));
+                    break;
                 }
             }
         }
@@ -36,13 +41,25 @@ public class DestructibleTiles : MonoBehaviour
         check = true;
         yield return new WaitForSeconds(0.9f);
         destructableTilemap.SetTile(destructableTilemap.WorldToCell(hitPosition), null);
-        StartCoroutine(BreakTileAnimation());
+        StartCoroutine(BreakTileAnimation(hitPosition));
         yield return null;
     }
 
-    private IEnumerator BreakTileAnimation()
+    private IEnumerator BreakTileAnimation(Vector3 hitPosition)
     {
-        // TODO
+        GameObject newBrokenTile = Instantiate(_brokenTile, hitPosition, Quaternion.identity);
+        newBrokenTile.SetActive(true);
+        SpriteRenderer brokenTileRenderer = newBrokenTile.GetComponent<SpriteRenderer>();
         yield return new WaitForEndOfFrame();
+        for (int i = 0; i < 10; i++)
+        {
+            newBrokenTile.transform.position -= new Vector3(0, 0.1f, 0);
+            Color color = brokenTileRenderer.color;
+            color.a -= 0.09f;
+            brokenTileRenderer.color = color;
+            yield return new WaitForEndOfFrame();
+        }
+        Destroy(newBrokenTile);
+        yield return null;
     }
 }
