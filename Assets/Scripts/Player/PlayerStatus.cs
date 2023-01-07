032eb08537e8e7746d49aeb0cb842fc9c8d8f075
Vexatone <vexatone160@gmail.com>
2023-01-08 00:22:27 +0900

    Fixed wall jump bug

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -7,6 +7,7 @@ class PlayerStatus : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D col)
     {
+        /*
         float delta_y = transform.position.y - col.GetContact(0).point.y;
         // 70도보다 큰 벽은 벽 판정
         // print($"Delta_y = {delta_y}, Threshold = {Radius * Mathf.Cos(70 * Mathf.Deg2Rad)}");
@@ -14,6 +15,21 @@ class PlayerStatus : MonoBehaviour
         {
             isOnGround = true;
         }
+        Debug.Log(delta_y);
+        */
+
+        bool groundContacts = false;
+        foreach (ContactPoint2D colPoint in col.contacts)
+        {
+            float delta_y = transform.position.y - colPoint.point.y;
+            if (delta_y > Radius * Mathf.Cos(70 * Mathf.Deg2Rad))
+            {
+                groundContacts = true;
+                break;
+            }
+        }
+
+        isOnGround = groundContacts;
     }
 
     private void OnCollisionExit2D()
