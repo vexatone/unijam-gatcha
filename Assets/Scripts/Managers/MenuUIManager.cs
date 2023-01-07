ddfe15a1e9cb38344a1eeadb65650d8359929ce7
Jeffrey Kim <vexatone160@gmail.com>
2023-01-08 06:16:26 +0900

    Added main menu UI

---
+++ Assets/Scripts/Managers/MenuUIManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+class MenuUIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    private void Start()
+    {
+        bgmSlider.value = SoundManager.Instance.BGMVolume;
+        sfxSlider.value = SoundManager.Instance.EffectVolume;
+        panel.SetActive(false);
+    }
+
+    public void SFXTest()
+    {
+        SoundManager.Instance.PlayEffect("");
+        // 이름은 알아서
+    }
+
+    public void Activate()
+    {
+        panel.SetActive(true);
+    }
+
+    public void Deactivate()
+    {
+        panel.SetActive(false);
+    }
+
+    public void UpdateBGMVolume()
+    {
+        SoundManager.Instance.BGMVolume = bgmSlider.value;
+    }
+
+    public void UpdateSFXVolume()
+    {
+        SoundManager.Instance.EffectVolume = sfxSlider.value;
+    }
+}
